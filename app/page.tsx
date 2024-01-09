a94255336cebfe50de09525d1f731d0f753f50c0
n-portfolio <141490012+n-portfolio@users.noreply.github.com>
2024-01-09 17:10:10 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -22,14 +22,13 @@ export default function Home() {
             Hello! I`m developer from Armenia, my name is Narek, I`m 16 years old
           </p>
           <div className="flex-col md:flex-row hidden md:flex gap-5">
+             <a href="/resume.pdf" download="Narek`s Resume">
             <Link
-              href="/my-skills"
               className="rounded-[20px]  group relative bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
             >
-              <a href="/resume.pdf" download="Narek`s Resume">
               Resume
-                </a>
             </Link>
+               </a>
         </div>
       </div>
 
