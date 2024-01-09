635b25303c9d0c510a9a04aa9fae925373531897
n-portfolio <141490012+n-portfolio@users.noreply.github.com>
2024-01-09 17:03:12 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -19,30 +19,15 @@ export default function Home() {
             </span>
           </h1>
           <p className="text-gray-200 z-[20]  md:block md:z-[0]">
-            Hello! I`m developer from Armenia, my name is Narek, I`m 17 years old
+            Hello! I`m developer from Armenia, my name is Narek, I`m 16 years old
           </p>
           <div className="flex-col md:flex-row hidden md:flex gap-5">
             <Link
               href="/my-skills"
               className="rounded-[20px]  group relative bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
             >
-              Learn more
+              Resume
             </Link>
-            <Link
-              href="/my-projects"
-              className="rounded-[20px] group relative bg-trasparent px-5 border border-white py-3 text-lg text-white max-w-[200px]"
-            >
-              <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hver:opacity-20" />
-              My projects
-            </Link>
-            <Link
-              href="/contact-me"
-              className="rounded-[20px] group relative bg-trasparent border border-white px-5 py-3 text-lg text-white max-w-[200px]"
-            >
-              <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hver:opacity-20" />
-              Contact me
-            </Link>
-          </div>
         </div>
       </div>
 
