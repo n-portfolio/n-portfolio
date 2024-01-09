ac6782fd3ea186110dfad2936ebe07eac373c2fa
n-portfolio <141490012+n-portfolio@users.noreply.github.com>
2024-01-09 17:25:37 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -33,28 +33,6 @@ export default function Home() {
           </div>
       </div>
 
-      <div className="absolute flex bottom-10 z-[20] right-5 flex-col md:hidden gap-5">
-        <Link
-          href="/my-skills"
-          className="rounded-[20px] group bg-blue-500 px-5 py-3 text-lg text-white max-w-[200px]"
-        >
-          Learn more
-        </Link>
-
-        <Link
-          href="/my-projects"
-          className="rounded-[20px] group bg-trasparent border border-white px-5 py-3 text-lg text-white max-w-[200px]"
-        >
-          My projects
-        </Link>
-        <Link
-          href="/contact-me"
-          className="rounded-[20px] group bg-trasparent border border-white px-5 py-3 text-lg text-white max-w-[200px]"
-        >
-          Contact me
-        </Link>
-      </div>
-
       <div className="absolute bottom-0 right-0 z-[10]">
         <Image
           src="/horse.png"
