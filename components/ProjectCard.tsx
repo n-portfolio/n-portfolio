7dc30f719d95603292eb42d7a4403a0e217ffa61
n-portfolio <141490012+n-portfolio@users.noreply.github.com>
2024-05-16 16:20:12 +0400

    Update ProjectCard.tsx

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useState } from 'react'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 
 interface Props {
     image: string;
@@ -9,50 +9,57 @@ interface Props {
     href: string;
 }
 
-const ProjectCard = ({ image, title, text, href}: Props) => {
+const ProjectCard = ({ image, title, text, href }: Props) => {
     const [isFlipped, setIsFlipped] = useState(false)
     const [isAnimating, setIsAnimating] = useState(false)
 
     function handleFlip() {
-        if(!isAnimating) {
+        if (!isAnimating) {
             setIsFlipped(!isFlipped)
             setIsAnimating(true)
         }
     }
-  return (
-    <div
-    onClick={handleFlip}
-    className='w-[250px] h-[150px] md:w-[450px] md:h-[280px] rounded-md cursor-pointer'>
-        <motion.div
-        className='flip-card-inner w-full h-full'
-        initial={false}
-        animate={{rotateY: isFlipped ? 180 : 360}}
-        transition={{ duration: 0.6, animationDirection: 'normal'}}
-        onAnimationComplete={() => setIsAnimating(false)}
-        >
-            <div
-            style={{backgroundImage: `url(${image})`}}
-            className='w-full h-full group relative flip-card-front bg-cover bg-center text-white rounded-lg p-4'>
-                    <div  className='absolute inset-0 w-full h-full rounded-md bg-black opacity-0 group-hover:opacity-40'/>
+
+    return (
+        <div
+            onClick={handleFlip}
+            className='w-[250px] h-[150px] md:w-[450px] md:h-[280px] rounded-md cursor-pointer'>
+            <motion.div
+                className='flip-card-inner w-full h-full'
+                initial={false}
+                animate={{ rotateY: isFlipped ? 180 : 360 }}
+                transition={{ duration: 0.6, animationDirection: 'normal' }}
+                onAnimationComplete={() => setIsAnimating(false)}
+            >
+                <div
+                    style={{ backgroundImage: `url(${image})` }}
+                    className='w-full h-full group relative flip-card-front bg-cover bg-center text-white rounded-lg p-4'>
+                    <div className='absolute inset-0 w-full h-full rounded-md bg-black opacity-0 group-hover:opacity-40' />
                     <div className='absolute inset-0 w-full h-full text-[20px] pb-10 hidden group-hover:flex items-center z-[20] justify-center'>
-                    Learn more &gt;
+                        Learn more &gt;
                     </div>
-            </div>
-            <div
-            style={{backgroundImage: `url(${image})`}}
-            className='w-full h-full group relative flip-card-back bg-cover bg-center text-white rounded-lg p-1'>
-                    <div  className='absolute inset-0 w-full h-full rounded-md bg-black opacity-50 z-[-1]'/>
-                  <div className='flex flex-col gap-10 md:gap-20 py-1 z-[30]'>
-                    <h1 className='text-whote text-[15] font-semibold'>{title}</h1>
-                    <p className='text-gray-200 text-[11px] md:text-[20px]'>
-                        {text} <a href={href} target="_blank" className='text-red-200'>Click to see</a>
-                    </p>
-                  </div>
-            </div>
-            <div className="w-[350px] h-[200px]"></div>
-        </motion.div>
-    </div>
-  )
+                </div>
+                <div
+                    style={{ backgroundImage: `url(${image})` }}
+                    className='w-full h-full group relative flip-card-back bg-cover bg-center text-white rounded-lg p-1'>
+                    <div className='absolute inset-0 w-full h-full rounded-md bg-black opacity-50 z-[-1]' />
+                    <div className='flex flex-col gap-10 md:gap-20 py-1 z-[30]'>
+                        <h1 className='text-whote text-[15] font-semibold'>{title}</h1>
+                        <p className='text-gray-200 text-[11px] md:text-[20px]'>
+                            {text} <a href={href} target="_blank" className='text-red-200'>Click to see</a>
+                        </p>
+                    </div>
+                </div>
+                <div className="w-[350px] h-[200px]"></div>
+            </motion.div>
+        </div>
+    )
 }
 
-export default ProjectCard
+const ProjectCardContainer = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className="overflow-auto max-h-[calc(100vh-200px)]">{children}</div>
+    );
+}
+
+export { ProjectCard, ProjectCardContainer };
